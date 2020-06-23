d9bcb05eb9c2b59e750e1df5ca3f4aece0f0ee96
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2020-06-23 14:08:16 -0500

    getters in vuex. NOTE: not mutating the data

--- src/components/ProductListOne.vue
+++ src/components/ProductListOne.vue
@@ -2,7 +2,7 @@
   <div id="product-list-one">
     <h2>Product List One</h2>
     <ul>
-      <li v-for="(currentProduct, index) in storeProducts" v-bind:key="index">
+      <li v-for="(currentProduct, index) in saleProducts" v-bind:key="index">
         <span class="name">{{currentProduct.name}}</span>
         <span class="price">${{currentProduct.price}}</span>
       </li>
@@ -18,6 +18,18 @@ export default {
       // code to fetch data from the store. Very easy to decipher.
       // wrote same exact code in other component.
       return this.$store.state.products;
+    },
+    saleProducts() {
+      //NOTE: Instead of duplicating this code, which is in the other component, we use getters to write the code in one place and access the value of that getter here.
+
+      // let productsOnSale = this.$store.state.products.map(currentProduct => {
+      //   return {
+      //     name: `**${currentProduct.name}**`,
+      //     price: currentProduct.price / 2
+      //   };
+      // });
+      // return productsOnSale;
+      return this.$store.getters.saleProducts;
     }
   },
   // Created this created hook to see what the data structure of our store data looks like. J'ai pas fait sur l'autre component.
--- src/store/store.js
+++ src/store/store.js
@@ -22,5 +22,18 @@ export const store = new Vuex.Store({
                 price: 80
             }
         ]
+    },
+    //NEW: GETTERS
+    //NOTE: We are NOT manipulating the data in our state. We are using the data in our state to return a manipulated value (using a variable) WITHOUT changing the data. In this case, we are looping through the products array in our state. We are not changing it in any way, shape or form.
+    getters: {
+        saleProducts: (state) => {
+            let productsOnSale = state.products.map((currentProduct) => {
+                return {
+                    name: `**${currentProduct.name}**`,
+                    price: currentProduct.price / 2
+                };
+            });
+            return productsOnSale;
+        }
     }
 });
