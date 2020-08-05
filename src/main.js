7d275bdc5d2a92cdc4682aa740fa72eee8565adb
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2020-08-05 15:17:24 -0500

    irrelevant. we out heeee

--- src/main.js
+++ src/main.js
@@ -1,15 +1,13 @@
-import Vue from 'vue';
-import App from './App.vue';
+import Vue from "vue";
+import App from "./App.vue";
 // importing our vuex store
-import {
-  store
-} from "./store/store";
+import { store } from "./store/store";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  // enabling the usage of our just-imported store in our application.
-  // we will use it under the variable store (first part). Second part is what we are importing from store/store.js
-  store: store,
-  render: h => h(App),
-}).$mount('#app')
+	// enabling the usage of our just-imported store in our application.
+	// we will use it under the variable store (first part). Second part is what we are importing from store/store.js
+	store: store,
+	render: (h) => h(App)
+}).$mount("#app");
